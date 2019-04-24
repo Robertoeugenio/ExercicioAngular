1b2b35575ae302ccdb0a8664caebd2484c8511a5
robertoeugenio1978@gmail.com <robertoeugenio>
2019-04-24 19:18:44 -0300

    primeiro exemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  nomes : string[] = ['joão', 'maria', 'josé', 'pedro', 'felipe', 'carlos'];
 }
