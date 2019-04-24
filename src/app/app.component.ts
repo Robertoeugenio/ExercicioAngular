1cf9fcba75f67e15f5bf6d3f45f5ee608a80b05e
robertoeugenio1978@gmail.com <robertoeugenio>
2019-04-24 20:23:32 -0300

    método de busca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nomes : string[] = [];
+  nomes: string[] = ['joão', 'maria', 'josé', 'pedro', 'felipe', 'carlos'];
+  nomesFiltro: string[];
+
+  /*busca(valor: string){
+    alert(valor)
+  }*/
+
+  buscar(valor: string) {
+    /* this.nomesFiltro = [];*/
+    //metodo 1 
+    /*for (var i = 0; i < this.nomes.length; i++) {
+      if (this.nomes[i].toLowerCase().includes(valor.toLowerCase())) {
+        this.nomesFiltro.push(this.nomes[i]);
+      }
+    }*/
+    //metodo 2
+    /* let temp =[];
+     this.nomes.forEach(buscarItem);
+     function buscarItem(nome){
+       if(nome.toLowerCase().includes(valor.toLocaleLowerCase())){
+         temp.push(nome);
+       }
+     }
+     this.nomesFiltro = temp;
+   }*/
+    /*metodo 3*/
+    this.nomesFiltro = this.nomes.filter(function (nome) {
+      return nome.toLocaleLowerCase().includes(valor.toLocaleLowerCase());
+    });
+  }
 }
